2ebbc1e17f67fa275bf6691b2787d26077608cea
Mohammed Salah <mohammed_salah@icloud.com>
2021-12-23 10:09:01 +0000

    added functionality for deleting a blog

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -2,15 +2,21 @@ import { useState } from 'react';
 import BlogList from './blogList';
 
 const Home = () => {
+
   const [blogs, setBlogs] = useState([
     { title: 'My new website', body: 'lorem ipsum', author: 'mo', id: 1 },
     { title: 'Welcome party', body: 'lorem ipsum', author: 'isra', id: 2 },
     { title: 'Web dev top tips', body: 'lorem ipsum', author: 'hoyo', id: 3 }
   ]);
 
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog => blog.id !== id);
+    setBlogs(newBlogs);
+  }
+
   return (  
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs!" />
+      <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
     </div>
   );  
 }
