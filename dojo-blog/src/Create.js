3d3b33907b4a8fa27d263a9e57745d8dc89f5381
Mohammed Salah <mohammed_salah@icloud.com>
2021-12-25 13:55:47 +0000

    added POST request

--- dojo-blog/src/Create.js
+++ dojo-blog/src/Create.js
@@ -4,16 +4,24 @@ const Create = () => {
   const[title, setTitle] = useState('');
   const[body, setBody] = useState('');
   const[author, setAuthor] = useState('mario');
+  const[isPending, setIsPending] = useState(false);
 
   const handleSubmit = (e) => { 
     e.preventDefault();
     const blog = { title, body, author };
 
-    console.log(blog);
 
+    setIsPending(true);
+
+    fetch('http://localhost:8000/blogs', {
+      method: 'POST',
+      headers: { "Content-Type": "application/json"},
+      body: JSON.stringify(blog)
+    }).then(() => {
+      console.log('new blog added')
+      setIsPending(false);
+    })
   }
-
-
 
   return (  
     <div className="create">
@@ -40,10 +48,8 @@ const Create = () => {
           <option value="mario">mario</option>
           <option value="mohammed">mohammed</option>
         </select>
-        <button>Add Blog</button>
-        <p> { title } </p>
-        <p> { body } </p>
-        <p> { author } </p>
+        { !isPending && <button>Add Blog</button> }
+        { isPending && <button disabled>Adding Blog...</button> }
       </form>
     </div>
   );
