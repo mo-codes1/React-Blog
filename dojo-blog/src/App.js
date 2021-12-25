7a614e3876a6b758a443a927a79efa3a9584bc77
Mohammed Salah <mohammed_salah@icloud.com>
2021-12-25 11:12:36 +0000

    added route parameters

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -2,6 +2,7 @@ import Navbar from './Navbar';
 import Home from './Home';
 import{ BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Create from './Create';
+import BlogDetails from './BlogDetails';
 
 function App() {
 
@@ -11,12 +12,19 @@ function App() {
         <Navbar />
         <div className="content">
           <Switch>
+
             <Route exact path="/">
               <Home />
             </Route>
+
             <Route path="/create">
               <Create/>
             </Route>
+
+            <Route path="/blogs/:id">
+              <BlogDetails/>
+            </Route>
+
           </Switch>
         </div>
       </div>
